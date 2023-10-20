6ab22f3f37ce9931ba1db82e33ce6407e5d73ebd
Rishabh Bhatt <bhatt.rishabh01@gmail.com>
2023-10-20 10:19:54 +0530

    add filter for Review Section

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,19 +1,78 @@
-import { review3, reviews2 } from '@/data/reviews';
-import ReviewBox from './ReviewBox';
-import AIButton from './AIButton';
+"use client";
+
+import { review3, reviews2 } from "@/data/reviews";
+import ReviewBox from "./ReviewBox";
+import { useMemo, useState } from "react";
+import { ActiveTagProps } from "@/types";
+import AIButton from "./AIButton";
+
+const FilterTags = ({
+  tags,
+  activeFilter,
+  setActiveFilter,
+}: ActiveTagProps) => {
+  const handleClick = (tag: string) => {
+    if (tag === activeFilter) return setActiveFilter("");
+    setActiveFilter(tag);
+  };
+
+  return (
+    <div className="flex gap-4 my-4">
+      {tags.map((tag) => {
+        const isActive = tag === activeFilter;
+        return (
+          <button
+            className={`px-3 py-1 rounded-full  shadow-md hover:shadow-sm  ${
+              isActive
+                ? "bg-green-200 hover:bg-green-300"
+                : "bg-orange-200 hover:bg-orange-300"
+            }`}
+            key={tag}
+            onClick={() => handleClick(tag)}
+          >
+            {tag}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
 const Reviews = () => {
-	const reviews = reviews2;
-	return (
-		<div className='bg-sky-50 p-5 rounded-md'>
-			<h2 className='text-2xl italic font-semibold text-sky-800 mb-4'>Reviews on the article :</h2>
-			<AIButton />
-			<ul className='flex gap-4 overflow-x-scroll'>
-				{reviews.map(review => (
-					<ReviewBox review={review} key={review.title}/>
-				))}
-			</ul>
-		</div>
-	);
+  const reviews = reviews2;
+
+  const [activeFilter, setActiveFilter] = useState("");
+
+  const sampleTags = ["knowledge", "generative ai", "engagement"];
+
+  const filteredResults = useMemo(() => {
+    if (!activeFilter) return reviews;
+    const regex = new RegExp(activeFilter, "i");
+    return reviews.filter((review) => regex.test(review.content));
+  }, [activeFilter]);
+
+  return (
+    <div className="bg-sky-50 p-5 rounded-md">
+      <h2 className="text-2xl italic font-semibold text-sky-800">
+        Reviews on the article :
+      </h2>
+      <AIButton />
+      <FilterTags
+        tags={sampleTags}
+        activeFilter={activeFilter}
+        setActiveFilter={setActiveFilter}
+      />
+      {activeFilter && (
+        <p className="font-semibold italic text-sm text-orange-800">
+          Showing {filteredResults.length} of {reviews.length} results :
+        </p>
+      )}
+      <ul className="flex gap-4 overflow-x-scroll">
+        {filteredResults.map((review) => (
+          <ReviewBox review={review} key={review.title} />
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default Reviews;
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,16 +1,17 @@
 const Tags = ({ tags }: { tags: string[] }) => {
 	return (
-		<div className="flex gap-3">
-			{tags.map((tag) => (
-				<div
-					key="tag"
-					className="bg-sky-200 text-sky-800 font-semibold px-3 py-1 rounded-md uppercase my-4 tracking-wider text-sm"
-				>
-					{tag}
-				</div>
-			))}
-		</div>
+	  <div className="flex gap-3">
+		{tags.map((tag) => (
+		  <div
+			key={tag}
+			className="bg-sky-200 text-sky-800 font-semibold px-3 py-1 rounded-md uppercase my-4 tracking-wider text-sm"
+		  >
+			{tag}
+		  </div>
+		))}
+	  </div>
 	);
-};
-
-export default Tags;
+  };
+  
+  export default Tags;
+  
